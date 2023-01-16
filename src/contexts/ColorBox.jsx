a933170f9c32ef2ec629d1325ea7e4890dac9d85
vitamin777 <rlaalsdn8@gmail.com>
2023-01-16 23:55:37 +0900

    refactor: ColorBox.jsx 에서 useContext 사용해 코드 간결하게수정
    -기존 Consumer 를 사용하지않고 useContext 로 ColorContext 의 state 에 접근해 color/subcolor 의 상태를 업데이트해주도록함

--- src/contexts/ColorBox.jsx
+++ src/contexts/ColorBox.jsx
@@ -1,31 +1,30 @@
-import React from "react";
-import { ColorConsumer } from "./Color";
+import React, {useContext} from "react";
+import ColorContext from "./Color";
 
 const ColorBox = () => {
+
+  const {state} = useContext(ColorContext)
+
   return (
     /*Consumer 사이에 중괄호를 열어 그안에 함수를 넣어주는 패턴
      * -Function as a child 또는 Render props 라고함
      * 컴포넌트의 children 이 있어야할 자리에 일반 jsx or 문자열이 아닌 함수를 전달하는것*/
-    <ColorConsumer>
-      {(value) => (
         <>
           <div
             style={{
               width: "64px",
               height: "64px",
-              background: value.state.color,
+              background: state.color,
             }}
           />
           <div
             style={{
               width: "32px",
               height: "32px",
-              background: value.state.subcolor,
+              background: state.subcolor,
             }}
           />
         </>
-      )}
-    </ColorConsumer>
   );
 };
 export default ColorBox;
